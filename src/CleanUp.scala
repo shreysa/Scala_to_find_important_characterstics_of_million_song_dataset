0563b452082572772e87a95d0f16e7f269076522
Shreysa <shreysa.89@gmail.com>
2017-10-27 09:46:27 -0400

    added artist name

--- src/CleanUp.scala
+++ src/CleanUp.scala
@@ -23,6 +23,7 @@ class CleanUp(row : String) extends java.io.Serializable {
     var artHot = 0.00
     var songHot = 0.00
     var title = ""
+    var artistName = ""
     var isValidRow = true;
 
     // Local logger
@@ -53,42 +54,11 @@ class CleanUp(row : String) extends java.io.Serializable {
     def getArtHot()   : Double = artHot
     def getSongHot()  : Double = songHot
     def getArtId()    : String = line(16)
+    def getArtistName : String = line(17)
     def getAlbum()    : String = line(22)
     def getSongId()   : String = line(23)
     def getTitle()    : String = line(24)
-
-      
-    // def getTopSong( x : rdd.RDD[Array[String]] ): Unit = {
-    //   var x1 = checkValidity("3", x)
-    //   var x2 = checkValidity("2", x1)
-    
-    //   var songtuple = x2.map { _(23) } zip x2.map { _(24) }             //(song_id, song_name) tuple
-    //   val loud = x2.map { _(6) }
-    //   val top5loud = songtuple.zip(loud).map { x => (x._1, x._2.toDouble) }.collect.toList.sortWith(_._2>_._2).take(5)
-    //   println("The top 5 loud songs are: " + top5loud)
-
-
-    //   x2 = checkValidity("4", x1)
-    //   songtuple = x2.map { _(23) } zip x2.map { _(24) }                 //(song_id, song_name) tuple
-    //   val long1 = x2.map { _(5) }
-    //   val top5long = songtuple.zip(long1).map { x => (x._1, x._2.toDouble) }.collect.toList.sortWith(_._2>_._2).take(5)
-    //   println("The top 5 long songs are: " + top5long)
-
-    //   x2 = checkValidity("5", x1)
-    //   songtuple = x2.map { _(23) } zip x2.map { _(24) }                  //(song_id, song_name) tuple
-    //   val tempo = x2.map { _(7) }
-    //   val top5fast = songtuple.zip(tempo).map { x => (x._1, x._2.toDouble) }.collect.toList.sortWith(_._2>_._2).take(5)
-    //   println("The top 5 fast songs are: " + top5fast)
-
-    //   x2 = checkValidity("11", x1)
-    //   songtuple = x2.map { _(23) } zip x2.map { _(24) }                  //(song_id, song_name) tuple
-    //   val hotns = x2.map { _(25) }
-    //   val top5hot = songtuple.zip(hotns).map { x => (x._1, x._2.toDouble) }.collect.toList.sortWith(_._2>_._2).distinct.take(5)
-    //   println("The top 5 hot songs are: " + top5hot)
-      
-    // }
-
-
+ 
     def checkValidity() : Boolean = {
       var result = false
      if(isValidRow && getDuration() > 0.00
@@ -100,37 +70,9 @@ class CleanUp(row : String) extends java.io.Serializable {
        && getArtHot() > 0
        && getSongHot() > 0
        && !(getAlbum.equalsIgnoreCase ("na"))
-       && !(getTitle().isEmpty) && !(getSongId().isEmpty) && !(getArtId().isEmpty)) {
+       && !(getTitle().isEmpty) && !(getSongId().isEmpty) && !(getArtId().isEmpty && !(getArtistName.isEmpty))) {
        result = true
      }
       result
      }
-
-    //  def checkValidity( a : String , data : rdd.RDD[Array[String]] ): rdd.RDD[Array[String]] = {
-    // a match {
-    //   case "1" => data.filter (x => ! (x(19).equalsIgnoreCase ("na")) )
-    //                 .filter (x => x(19).toDouble > 0)                     //Familiarity
-    //   case "2" => data.filter (x => !(x(19).equalsIgnoreCase ("na")) )
-    //                 .filter{_(6).toDouble < 0}                              //Loudness
-    //   case "3" => data.filter (x => ! (x(0).isEmpty) )                      //Track_id
-    //   case "4" => data.filter (x => x(5).toDouble > 0)                       //duration
-    //   case "5" => data.filter (x => ! (x(7).equalsIgnoreCase ("na") ) )
-    //                 .filter (x => x(9).toDouble > 0)                         //tempo
-
-    //   case "6" => data.filter (x => (x(8).toInt >= 0) && (x(8).toInt <= 11) )       //key
-
-    //   case "7" => data.filter (x => ! (x(9).equalsIgnoreCase ("na") ) )         //key_conf
-    //                 .filter (x => x(9).toDouble > 0.7)
-
-    //   case "8" => data.filter (x => ! (x(16).isEmpty))                                    //artist_id
-
-    //   case "9" => data.filter (x => ! (x(20).equalsIgnoreCase ("na")))
-    //                 .filter (x => (x(20).toDouble >= 0) && (x(20).toDouble <= 1))                //artist_hotness
-
-    //   case "10" => data.filter (x => ! (x(24).isEmpty) )                    //title
-    //   case "11" => data.filter (x => ! (x(25).equalsIgnoreCase ("na") ) )
-    //                   .filter (x => (x(25).toDouble >= 0) && (x(25).toDouble <= 1))                //song_hotness
-       
-    // }
-
 }
